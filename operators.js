063b88f49ca80c56b6c5c252e263069f8e3ad6cf
Annashruthi <annashruthi05@gmail>
2025-06-19 10:54:46 +0530

    complete  operators topic

---
+++ operators.js
@@ -0,0 +1,15 @@
+console.log(10+3);
+console.log(10-2);
+console.log(10/2);
+console.log(10*2);
+console.log(10>2);
+console.log(10<2);
+console.log(10<=2)
+console.log(10>=2);
+console.log("2"== 2);
+console.log("2" === 2);
+console.log("2" !== 2);
+console.log(2>3 && 3>2);
+console.log(2<3 && 3<2);
+console.log(3>2 || 2>3);
+console.log(2<3 || 3<2);
