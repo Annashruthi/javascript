f7d82961568ac2187a92a00639089e8d852c0309
Annashruthi <annashruthi05@gmail>
2025-06-19 11:21:58 +0530

    complete condition topics

---
+++ condition.js
@@ -0,0 +1,13 @@
+let marks = 85;
+if(marks >= 90){
+    console.log("A grade");
+}else if(marks >=75){
+    console.log("B grade");
+}else{
+    console.log("c grade");
+}
+if(1){
+    console.log("Working");
+}else{
+    console.llog("False value");
+}
